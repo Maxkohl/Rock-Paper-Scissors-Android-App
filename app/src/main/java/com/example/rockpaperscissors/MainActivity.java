4c6131714220455cb39e5537f9f8b2b6dc8c69a1
Max Kohl <Maxdkohl@gmail.com>
2020-02-29 20:50:24 -0500

    Added images to user and computer choice

--- app/src/main/java/com/example/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.java
@@ -13,30 +13,34 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView input, output;
+    ImageView userChoice, compChoice;
     Button rock, paper, scissors;
-    TextView score;
+    TextView score, message;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) { //This is where you initialize activity
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        score.setText("Your score: " + Integer.toString(playerWins) + " Computer Score: " + Integer.toString(computerWins));
 
 
-        input = (ImageView) findViewById(R.id.iv_input);
+        userChoice = (ImageView) findViewById(R.id.iv_userChoice);
+        compChoice = (ImageView) findViewById(R.id.iv_compChoice);
         rock = (Button) findViewById(R.id.btn_rock);
         paper = (Button) findViewById(R.id.btn_paper);
         scissors = (Button) findViewById(R.id.btn_scissors);
         score = (TextView) findViewById(R.id.tv_score);
 
+        compChoice.setImageResource(R.drawable.rock);
+        userChoice.setImageResource(R.drawable.scissors);
+
         rock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                userChoice.setImageResource(R.drawable.rock);
                String message = playTurn(1);
                 Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
-                score.setText("Your score: " + Integer.toString(playerWins) + " Computer Score: " + Integer.toString(computerWins));
+                score.setText("Your score: " + Integer.toString(playerWins) + "     Computer Score: " + Integer.toString(computerWins));
 
             }
         });
@@ -44,18 +48,20 @@ public class MainActivity extends AppCompatActivity {
         paper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                userChoice.setImageResource(R.drawable.paper);
                 String message = playTurn(2);
                 Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
-                score.setText("Your score: " + Integer.toString(playerWins) + " Computer Score: " + Integer.toString(computerWins));
+                score.setText("Your score: " + Integer.toString(playerWins) + "     Computer Score: " + Integer.toString(computerWins));
             }
         });
 
         scissors.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                userChoice.setImageResource(R.drawable.scissors);
                 String message = playTurn(3);
                 Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
-                score.setText("Your score: " + Integer.toString(playerWins) + " Computer Score: " + Integer.toString(computerWins));
+                score.setText("Your score: " + Integer.toString(playerWins) + "     Computer Score: " + Integer.toString(computerWins));
             }
         });
 
@@ -69,6 +75,14 @@ public class MainActivity extends AppCompatActivity {
         Random randomNum = new Random();
 
         int computerChoice = randomNum.nextInt(3) + 1;
+
+        if (computerChoice == 1) {
+            compChoice.setImageResource(R.drawable.rock);
+        } else if (computerChoice == 2) {
+            compChoice.setImageResource(R.drawable.paper);
+        } else {
+            compChoice.setImageResource(R.drawable.scissors);
+        }
 
         if (playerChoice == computerChoice) {
             // Tie
